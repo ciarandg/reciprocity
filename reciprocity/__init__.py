6aa2f63e18238e22782ceb2eb04ce68b927b8bb5
Ciaran De Groot <ciaran.deg@gmail.com>
2026-02-14 23:41:44 -0700

    Use ollama

--- reciprocity/__init__.py
+++ reciprocity/__init__.py
@@ -1,4 +1,8 @@
+from ollama import ChatResponse, Client
 import typer
+
+OLLAMA_HOST = "http://127.0.0.1:11434"
+OLLAMA_MODEL = "qwen3:1.7b"
 
 app = typer.Typer()
 
@@ -11,6 +15,17 @@ def run(
     if verbose:
         typer.echo("Verbose mode on")
     typer.echo(f"Processing {input}")
+    client = Client(host=OLLAMA_HOST)
+    response: ChatResponse = client.chat(
+        model=OLLAMA_MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is the sky blue? In 10 words",
+            },
+        ],
+    )
+    print(response.message.content)
 
 
 def main():
