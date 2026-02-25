f4e3235b6a1ffb4be755597b750ae687c0c841df
Ciaran De Groot <ciaran.deg@gmail.com>
2026-02-24 22:07:17 -0400

    Add reciprocity version to model tag

--- reciprocity/globals.py
+++ reciprocity/globals.py
@@ -1,5 +1,8 @@
+from importlib.metadata import version
 from reciprocity.config import config
 from ollama import Client
 
 ollama_client = Client(host=config.ollama.host)
-ollama_model = f"reciprocity:{config.ollama.base.replace(':', '-')}"
+ollama_model = (
+    f"reciprocity:{version('reciprocity')}-{config.ollama.base.replace(':', '-')}"
+)
